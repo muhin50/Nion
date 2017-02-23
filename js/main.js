ef521df5caacfc1f3059ec9a303a00a2e7936245
muhin50 <muhinforex@gmail.com>
2017-02-23 23:36:05 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	$('.bd22,.bd33').click(function(){
+		$(this).toggleClass('open');
+	});
+    	$('.bd22,.bd33').click(function(){
+		$(".navbar-right").fadeToggle();
+	});
+
+}); 
